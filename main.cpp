844bf275d617cd64d754825855bb2fa3b3b49873
Gray <phungngoctho@gmail.com>
2020-12-16 15:22:39 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     a.setAttribute(Qt::AA_UseHighDpiPixmaps);
     a.setWindowIcon(QIcon(":/images/zalo.svg"));
-    QString yDefaultDesc = QString("<span style=' font-size:8pt; font-weight:600;'>This is the simple app with dtk lib and WebApp Runtime. Customize by yaua</span>");
+    QString yDefaultDesc = QString("<span style=' font-size:8pt; font-weight:600;'>This is the simple app with Webengineview. Customize by yaua</span>");
     QString yTitle = "Zalo";
     QString yUrl = "https://chat.zalo.me/";
     int width = 700;
