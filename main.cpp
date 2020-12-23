245eb53d025bdf3c84da05795947d11947b27142
Gray <phungngoctho@gmail.com>
2020-12-23 16:04:32 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,12 +6,11 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     a.setAttribute(Qt::AA_UseHighDpiPixmaps);
     a.setWindowIcon(QIcon(":/images/zalo.svg"));
-    //QString yDefaultDesc = QString("<span style=' font-size:8pt; font-weight:600;'>This is the simple app with Webengineview. Customize by yaua</span>");
-    QString yTitle = "Zalo";
     QString yUrl = "https://chat.zalo.me/";
     int width = 700;
     int height = 800;
-    MainWindow w(yTitle, yUrl, width, height);
+    MainWindow w(yUrl, width, height);
     w.show();
+    w.setWindowTitle("Zalo web chat");
     return a.exec();
 }
